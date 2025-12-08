7a10629a321c7d36f2ac59d68bd7854037553011
rukhshanarifm <rukhshanarifm@gmail.com>
2025-12-08 18:06:01 +0500

    for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-dashboard-v1/',
   root: '.',
   server: {
     port: 3000,
@@ -11,7 +12,7 @@ export default defineConfig({
     open: true,
   },
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     sourcemap: true,
   },
 })
