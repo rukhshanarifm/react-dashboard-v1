e24120ee17c6bfcc2511ad92f95c6b8b13058326
rukhshanarifm <rukhshanarifm@gmail.com>
2025-12-08 18:09:44 +0500

    updated deployment

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/react-dashboard-v1/',
-  root: '.',
+  base: mode === 'production' ? '/react-dashboard-v1/' : '/',
   server: {
     port: 3000,
-    strictPort: true,
-    open: true,
+    strictPort: false,
   },
   build: {
     outDir: 'dist',
     sourcemap: true,
   },
-})
+}))
